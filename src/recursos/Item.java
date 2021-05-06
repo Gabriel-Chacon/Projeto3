b0a5600df53fcfe3fe69e787c685bc3874493b9d
luccaki <49926738+luccaki@users.noreply.github.com>
2021-05-06 13:55:15 -0300

    Add files via upload

--- src/recursos/Item.java
+++ src/recursos/Item.java
@@ -6,8 +6,8 @@ public class Item {
     private long tamanho;
     
     public Item(long tempo,long bytes){
-        this.tempo=tempo;
-        this.tamanho=bytes;
+        this.tempo = tempo;
+        this.tamanho = bytes;
     }
     
     public long getTempo(){
@@ -18,12 +18,11 @@ public class Item {
     }
     
     void alterarTempo(long tempo){
-        this.tempo=tempo;
+        this.tempo = tempo;
     }
     
     @Override
     public String toString(){
         return String.format("Tempo: %d, Tamanho: %d", tempo,tamanho);
-
     }
 }
